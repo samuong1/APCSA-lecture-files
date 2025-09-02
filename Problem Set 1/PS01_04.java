3ce17dec6e9c0105b71379ef79d3f09b355284e4
Sam Uong <samsamuong@gmail.com>
2025-09-02 10:05:06 -0500

    Changed scanner to input to make it less confusing

--- Problem Set 1/PS01_04.java
+++ Problem Set 1/PS01_04.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class PS01_04{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter the subtotal (ex. 10.50) >>> ");
-        double subtotal = scanner.nextDouble();
+        double subtotal = input.nextDouble();
         System.out.print("Enter the percent gratuity (ex. 15 for 15%) >>> ");
-        double gratuity = scanner.nextDouble();
+        double gratuity = input.nextDouble();
         double finalGratuity = (gratuity / 100) * subtotal;
         double finalTotal = subtotal * finalGratuity;
         System.out.println("The gratuity is $" + finalGratuity + " and the total is $" + finalTotal);
--- Problem Set 1/PS01_06.java
+++ Problem Set 1/PS01_06.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 public class PS01_06{
     public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter a positive integer that is less than 1000 >>> ");
-        int chosenInteger = scanner.nextInt();
+        int chosenInteger = input.nextInt();
         int digitOne = chosenInteger % 10;
         chosenInteger = chosenInteger / 10;
         int digitTwo = chosenInteger % 10;
