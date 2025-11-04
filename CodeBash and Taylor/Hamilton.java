c447bcfecbc58f6f3a1bbd9682a7af5dd021298e
Sam Uong <samsamuong@gmail.com>
2025-11-04 10:24:41 -0600

    FIxing Hamilton?

--- CodeBash and Taylor/Hamilton.java
+++ CodeBash and Taylor/Hamilton.java
@@ -4,15 +4,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Hamilton {
-    int accountNumber = 0;
+    long accountNumber = 0;
     int accountPosition = 0;
     double money = 0.0;
     boolean isSavings = false;
-    private static List<Hamilton> accountList = new ArrayList<>();
+    public static List<Hamilton> accountList = new ArrayList<>();
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(new File("Hamilton.dat"));
-        int count = scanner.nextInt();
-        for (int i = 0; i < count; i++){
+        //int count = scanner.nextInt();
+        accountOpen(123456, 1500, true);
+        accountOpen(123456, 1500, true);
+        for (int i = 0; i < 4; i++){
             if(scanner.next() == "OPEN"){
                 //account open method
             }
@@ -21,24 +23,35 @@ public class Hamilton {
             }
         }
     }
-    public Hamilton(int accountNumber, double money, boolean isSavings){
+    private Hamilton(long accountNumber, double money, boolean isSavings){
         this.accountNumber = accountNumber;
         this.money = money;
         this.isSavings = isSavings;
     }
-    public static void accountOpen(int accountN, double monies, boolean isItSavings){
+    public static void accountOpen(long accountN, double monies, boolean isItSavings){
         Hamilton newAccount = new Hamilton(accountN, monies, isItSavings);
         for (int z = 0; z < accountList.size(); z++){
             if(accountList.get(z).accountNumber == newAccount.accountNumber){
-                for (int i = 0; i < 31; i++) {               
-                    if (accountList.add(newAccount)) {               
-                    break;
+                for (int i = 0; i < 31; i++) {     
+                    String binaryAccountNumber = Long.toBinaryString(newAccount.accountNumber).trim();
+                    int length = binaryAccountNumber.length();
+                    if (i > 0){
+                        if (binaryAccountNumber.charAt(length - i) == 1){
+                            binaryAccountNumber = binaryAccountNumber.substring(0, length - i) + "0";
+                        }   
+                        else{
+                            binaryAccountNumber = binaryAccountNumber.substring(0, length - i) + "1";
+                        }     
+                    }
+                    newAccount.accountNumber = Long.parseLong(binaryAccountNumber, 2);
+                    if (accountList.add(newAccount)) {            
+                        break;
                     }   
                 }
             }
-        accountList.add(newAccount);
-        System.out.println(accountN);
         }
+        accountList.add(newAccount);
+        System.out.println(newAccount.accountNumber);
     }
     public double accountWithdraw(int accountNo, double monies2){
         return money;
